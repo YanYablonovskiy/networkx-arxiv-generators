38d5591f4acd9ccc219535fe0488c307caf33260
YanYablonovskiy <yablonovskiy.yan@gmail.com>
2025-09-07 13:37:02 +1000

    Refactor test script and add new graph function call
    Wrapped test code in a function and added a call to mckay_wormald_simple_graph_from_graph for additional testing. This improves code organization and expands test coverage for the mckay_wormald module.

--- tests/test_mckay_wormald.py
+++ tests/test_mckay_wormald.py
@@ -1,15 +1,22 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 from nx_arxivgen.generators.mckay_wormald import mckay_wormald_simple_graph
+from nx_arxivgen.generators.mckay_wormald import mckay_wormald_simple_graph_from_graph
 from nx_arxivgen.generators.mckay_wormald import mckay_random_graph_encoding #type: ignore
-test_deg_seq = [1, 1, 2, 3, 3, 2, 6, 6,7,8,6,1,2,3,4,5,6,7,8,9,10]
 
-test : nx.Graph = mckay_wormald_simple_graph(test_deg_seq, debug=True)
-nx.draw_spring(test, with_labels=True)
-plt.show()
+def test() -> None:
+    test_deg_seq = [1, 1, 2, 3, 3, 2, 6, 6,7,8,6,1,2,3,4,5,6,7,8,9,10]
 
-sample_graph = nx.generators.binomial_graph(20, 0.5)
+    test : nx.Graph = mckay_wormald_simple_graph(test_deg_seq, debug=True)
+    nx.draw_spring(test, with_labels=True)
+    plt.show()
 
-sample_graph = nx.generators.binomial_graph(20, 0.5)
+    sample_graph = nx.generators.binomial_graph(20, 0.5)
 
-print(mckay_random_graph_encoding(sample_graph))
+    sample_graph = nx.generators.binomial_graph(20, 0.5)
+
+    print(mckay_wormald_simple_graph_from_graph(sample_graph))
+
+    print(mckay_random_graph_encoding(sample_graph))
+
+test()
